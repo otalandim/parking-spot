f5ee9fd40e2adf3ed9138479a40bd2edccb7b0f1
Otávio Landim <otavio.landim@gmail.com>
2022-09-22 00:43:07 -0300

    Adding ParkingSpotController

---
+++ src/main/java/com/api/parkingspot/controllers/ParkingSpotController.java
@@ -0,0 +1,16 @@
+package com.api.parkingspot.controllers;
+
+import com.api.parkingspot.services.ParkingSpotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+    @Autowired
+    private ParkingSpotService service;
+}
