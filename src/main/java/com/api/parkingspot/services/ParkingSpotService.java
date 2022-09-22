a523f682cc66f5fc429c7e078aa7d3e49c1bd88d
Otávio Landim <otavio.landim@gmail.com>
2022-09-22 01:12:21 -0300

    Adding save method to ParkingSpotService

--- src/main/java/com/api/parkingspot/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingspot/services/ParkingSpotService.java
@@ -1,12 +1,21 @@
 package com.api.parkingspot.services;
 
+import com.api.parkingspot.entities.ParkingSpot;
 import com.api.parkingspot.repositories.ParkingSpotRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
 
     @Autowired
     private ParkingSpotRepository repository;
+
+    @Transactional
+    public ParkingSpot save(ParkingSpot entity) {
+        return repository.save(entity);
+    }
 }
