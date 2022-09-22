31947842730824e6b30806e5053dd4dea9405840
Otávio Landim <otavio.landim@gmail.com>
2022-09-22 01:03:34 -0300

    Adding POST router to ParkingSpotController

--- src/main/java/com/api/parkingspot/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingspot/controllers/ParkingSpotController.java
@@ -1,10 +1,21 @@
 package com.api.parkingspot.controllers;
 
+import com.api.parkingspot.dtos.ParkingSpotDto;
+import com.api.parkingspot.entities.ParkingSpot;
 import com.api.parkingspot.services.ParkingSpotService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -13,4 +24,12 @@ public class ParkingSpotController {
 
     @Autowired
     private ParkingSpotService service;
+
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto dto) {
+        var entity = new ParkingSpot();
+        BeanUtils.copyProperties(dto, entity);
+        entity.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(entity));
+    }
 }
